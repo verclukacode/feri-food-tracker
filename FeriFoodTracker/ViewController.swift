a784777843db1626890efb0a24013bb762ae7623
vercluka <lukanaguglu@gmail.com>
2025-11-07 11:39:09 +0100

    Init

---
+++ FeriFoodTracker/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FeriFoodTracker
+//
+//  Created by Luka Verč on 7. 11. 25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
